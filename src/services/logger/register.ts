05969f8f9e48d8fc1f418ea131d0aa4022e56e50
Brent Honeybone <brent@glacial.nz>
2020-11-10 13:14:25 +0100

    chore: add environment for logger type

--- src/services/logger/register.ts
+++ src/services/logger/register.ts
@@ -1,9 +1,16 @@
 import { Context } from '../../vision/types'
 import { Container as IContainer } from '../container/types'
+import { Environment } from '../environment/Environment'
 import { ElixirLogger } from './Logger'
+import { LogType } from './types'
 
 export default (ctx: Context): void => {
   const Container: IContainer = ctx.elixir.services('Container')
 
-  Container.singleton('Logger', new ElixirLogger())
+  Container.singleton(
+    'Logger',
+    new ElixirLogger(
+      Environment.get('LOGGER_TYPE', LogType.CONSOLE) as LogType,
+    ),
+  )
 }
